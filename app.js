a256652dbbfd36bb7f7f457122f7e47890e94c37
change-coder <80451758+change-coder@users.noreply.github.com>
2021-03-18 13:08:23 +0530

    loading screen

---
+++ app.js
@@ -0,0 +1,25 @@
+const logo =document.querySelectorAll('#logo path');
+
+// for(let i =0;i<logo.length;i++)
+// {
+//     console.log(`letter ${i} is ${logo[i].getTotalLength} `);
+// }
+
+// var myPath = document.getElementById("p6");
+// var p6 = myPath.getTotalLength();
+// console.log(p6);  value = 892.3404541015625
+
+
+
+
+// myPath = document.getElementById("p7");
+// var p7 = myPath.getTotalLength();
+// console.log(p7);  value= 1005.4531860351562
+ 
+// myPath = document.getElementById("p8");
+// var p8 = myPath.getTotalLength();
+// console.log(p8); value = 1110.7091064453125
+
+// myPath = document.getElementById("p9");
+// var p9 = myPath.getTotalLength();
+// console.log(p9); value = 1223.82275390625
